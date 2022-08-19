86ddf2f37f26a92d046a01483e3440d8eb8b1b2d
mgvictoriano <mgvictoriano@live.com>
2022-08-19 10:33:41 -0300

    Initial commit

---
+++ src/test/java/com/br/dio/cloud/parking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dio.cloud.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
